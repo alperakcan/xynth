bd1d5a86828bf2eb0cf4c0d17bc0df4fbef3630e
anhanguera <anhanguera@777c6629-d510-0410-a57e-d487f2a3d22b>
2006-07-19 16:56:10 +0000

    ...

--- trunk/ports/kaffe/xynth/window.c
+++ trunk/ports/kaffe/xynth/window.c
@@ -3,9 +3,11 @@
 
 void Java_java_awt_Toolkit_wndSetFrameInsets (JNIEnv* env UNUSED, jclass clazz UNUSED, jint top, jint left, jint bottom, jint right)
 {
+	DEBUGF("Enter");
 	xynth->frame_insets.top  = top;
 	xynth->frame_insets.left = left;
 	xynth->frame_insets.bottom = bottom;
 	xynth->frame_insets.right = right;
 	xynth->frame_insets.guess = 1;
+	DEBUGF("Leave");
 }
