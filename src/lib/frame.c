246b7a4157f8532c3794e44ba0fbeef7993797d6
Alper Akcan <alper.akcan@gmail.com>
2006-09-08 21:26:52 +0000

    add file headers

--- src/lib/frame.c
+++ src/lib/frame.c
@@ -1,3 +1,17 @@
+/***************************************************************************
+    begin                : Fri Sep 8 2006
+    copyright            : (C) 2006 by Alper Akcan
+    email                : distchx@yahoo.com
+  ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "xynth_.h"
 
