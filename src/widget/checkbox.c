41617cc01554f173f38b66d491e95eb4f0537c56
Alper Akcan <alper.akcan@gmail.com>
2006-10-10 15:19:49 +0000

    ...

--- src/widget/checkbox.c
+++ src/widget/checkbox.c
@@ -1,3 +1,17 @@
+/***************************************************************************
+    begin                : Tue Oct 10 2006
+    copyright            : (C) 2006 by Alper Akcan
+    email                : distchx@yahoo.com
+  ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "../lib/xynth_.h"
 #include "widget.h"
