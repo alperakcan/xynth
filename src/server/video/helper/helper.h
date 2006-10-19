3bfa4e5a5dc68459d970601a6f611d5372f37861
Alper Akcan <alper.akcan@gmail.com>
2006-10-19 14:17:11 +0000

    ...

--- src/server/video/helper/helper.h
+++ src/server/video/helper/helper.h
@@ -20,7 +20,7 @@ int s_video_helper_console_switch (int vt);
 #endif /* VIDEO_HELPER_CONSOLE */
 
 #if defined(VIDEO_HELPER_IRMAN)
-#include "irman"
+#include "irman.h"
 #endif /* VIDEO_HERLPER_IRMAN */
 
 #if defined(VIDEO_HELPER_KBD)
