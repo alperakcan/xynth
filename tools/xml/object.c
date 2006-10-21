999467831c601793fcd65332df67adca2d6aa3f0
Alper Akcan <alper.akcan@gmail.com>
2006-10-21 22:30:16 +0000

    ...

--- tools/xml/object.c
+++ tools/xml/object.c
@@ -1,23 +1,79 @@
 
+#include <stdlib.h>
 #include <xynth.h>
 #include <widget.h>
 
-void red_draw (w_object_t *object)
+static int area_hide = 0;
+
+extern w_frame_t *area;
+extern w_frame_t *frame_red;
+extern w_frame_t *frame_green;
+extern w_frame_t *frame_blue;
+extern w_frame_t *frame_white;
+
+extern w_textbox_t *textbox_hide;
+
+void frame_red_draw (w_object_t *object)
 {
 	s_fillbox(object->surface, 0, 0, object->surface->width, object->surface->height, s_rgbcolor(object->surface, 255, 0, 0));
 }
 
-void green_draw (w_object_t *object)
+void frame_green_draw (w_object_t *object)
 {
 	s_fillbox(object->surface, 0, 0, object->surface->width, object->surface->height, s_rgbcolor(object->surface, 0, 255, 0));
 }
 
-void blue_draw (w_object_t *object)
+void frame_blue_draw (w_object_t *object)
 {
 	s_fillbox(object->surface, 0, 0, object->surface->width, object->surface->height, s_rgbcolor(object->surface, 0, 0, 255));
 }
 
-void white_draw (w_object_t *object)
+void frame_white_draw (w_object_t *object)
 {
 	s_fillbox(object->surface, 0, 0, object->surface->width, object->surface->height, s_rgbcolor(object->surface, 255, 255, 255));
+}
+
+void button_hide_draw (w_object_t *object)
+{
+	if (area_hide) {
+		w_textbox_set_str(textbox_hide->object, "show area");
+	} else {
+		w_textbox_set_str(textbox_hide->object, "hide area");
+	}
+}
+
+void button_hide_pressed (w_object_t *object, int button)
+{
+	area_hide++;
+	area_hide = (area_hide) % 2;
+	if (area_hide) {
+		w_object_hide(area->object);
+	} else {
+		w_object_show(area->object);
+	}
+}
+
+void button_blender_pressed (w_object_t *object, int button)
+{
+	w_object_move(frame_red->object, (rand() + 1) % area->object->surface->buf->w, (rand() + 1) % area->object->surface->buf->h,
+	                                 (rand() + 1) % area->object->surface->buf->w, (rand() + 1) % area->object->surface->buf->h);
+	w_object_move(frame_green->object, (rand() + 1) % area->object->surface->buf->w, (rand() + 1) % area->object->surface->buf->h,
+	                                   (rand() + 1) % area->object->surface->buf->w, (rand() + 1) % area->object->surface->buf->h);
+	w_object_move(frame_blue->object, (rand() + 1) % area->object->surface->buf->w, (rand() + 1) % area->object->surface->buf->h,
+	                                  (rand() + 1) % area->object->surface->buf->w, (rand() + 1) % area->object->surface->buf->h);
+	w_object_move(frame_white->object, (rand() + 1) % area->object->surface->buf->w, (rand() + 1) % area->object->surface->buf->h,
+	                                   (rand() + 1) % area->object->surface->buf->w, (rand() + 1) % area->object->surface->buf->h);
+}
+
+void button_focus_pressed (w_object_t *object, int button)
+{
+	int r;
+	w_frame_t *obj[] = {
+		frame_red,
+		frame_green,
+		frame_blue,
+		frame_white
+	};
+	r = rand() % 4;
+	w_object_show(obj[r]->object);
 }
