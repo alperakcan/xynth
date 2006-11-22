184c06db0ad0f573487a523e7ac61ee56ce96334
anhanguera <anhanguera@777c6629-d510-0410-a57e-d487f2a3d22b>
2006-11-22 15:10:38 +0000

    irserial

---
+++ trunk/src/server/video/helper/irserial.c
@@ -0,0 +1,62 @@
+
+#if defined(VIDEO_HELPER_IRSERIAL)
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+#define IR_CODE_LEN 255
+static int irserial_fd = -1;
+unsigned int irserial_time;
+static char irserial_buf[IR_CODE_LEN];
+static struct termios irserial_oldtio;
+static struct termios irserial_newtio;
+
+int irserial_init (char *dev, unsigned int brate)
+{
+        irserial_fd = open(dev, O_RDWR | O_NOCTTY ); 
+        if (irserial_fd < 0) {
+        	printf("could not open file %s\n", dev);
+        	return irserial_fd;
+        }
+        tcgetattr(irserial_fd, &irserial_oldtio);
+        memset(&irserial_newtio, 0, sizeof(irserial_newtio));
+        irserial_newtio.c_cflag = brate | CRTSCTS | CS8 | CLOCAL | CREAD;
+        irserial_newtio.c_iflag = IGNPAR;
+        irserial_newtio.c_oflag = 0;
+        irserial_newtio.c_lflag = 0;
+        irserial_newtio.c_cc[VTIME] = 0;
+        irserial_newtio.c_cc[VMIN] = 5;
+        tcflush(irserial_fd, TCIFLUSH);
+        tcsetattr(irserial_fd, TCSANOW, &irserial_newtio);
+	return irserial_fd;
+}
+
+int irserial_uninit (void)
+{
+        tcsetattr(irserial_fd, TCSANOW, &irserial_oldtio);
+        close(irserial_fd);
+	return 0;
+}
+
+char * irserial_getcode (void)
+{
+	int len;
+	unsigned int t;
+	len = read(irserial_fd, irserial_buf, IR_CODE_LEN - 1);
+	if (len > 0) {
+		t = time(NULL);
+		if (irserial_time < t) {
+			irserial_time = t;
+			return irserial_buf;
+		}
+	}
+	return NULL;
+}
+
+#endif
