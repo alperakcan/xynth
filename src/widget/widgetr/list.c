40c3fcdadfd54191a80a0f3448a7943b6e2b2faf
Alper Akcan <alper.akcan@gmail.com>
2006-12-20 19:45:59 +0000

    ...

--- src/widget/widgetr/list.c
+++ src/widget/widgetr/list.c
@@ -1,2 +1,16 @@
+/***************************************************************************
+    begin                : Wed Dec 20 2006
+    copyright            : (C) 2006 by Alper Akcan
+    email                : distchx@yahoo.com
+  ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "../widgetc/list.c"
