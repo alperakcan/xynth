f0d071993ad3032f0e7f500bc6e1dbb19fc2131c
anhanguera <anhanguera@777c6629-d510-0410-a57e-d487f2a3d22b>
2006-12-20 19:15:36 +0000

    ...

--- trunk/src/widget/widgetr/widgetr.c
+++ trunk/src/widget/widgetr/widgetr.c
@@ -3,23 +3,9 @@
 #include "widget.h"
 #include <getopt.h>
 
-typedef struct w_node_s w_node_t;
-typedef struct w_table_s w_table_t;
+#include "table.h"
+
 typedef struct w_widgetr_s w_widgetr_t;
-
-struct w_node_s {
-	unsigned long int key;
-	char *name;
-	w_node_t *next;
-	w_node_t *prev;
-	w_table_t *table;
-	void *data;
-};
-
-struct w_table_s {
-	w_table_t *table;
-	w_node_t *node;
-};
 
 struct w_widgetr_s {
 	unsigned int depth;
@@ -58,195 +44,6 @@ int axtoi (char *hex)
 	return (intvalue);
 }
 
-int cmp_string (char *str, char *ptr)
-{
-	while (*str && *ptr) {
-		if (*str++ != *ptr++) {
-			return -1;
-		}
-	}
-	if (*str || *ptr) {
-		return -1;
-	}
-	return 0;
-}
-
-unsigned long int hash_string (const char *str_param)
-{
-	#define HASHWORDBITS 32
-	unsigned long int hval, g;
-	const char *str = str_param;
-	hval = 0;
-	while (*str != '\0') {
-		hval <<= 4;
-		hval += (unsigned char) *str++;
-		g = hval & ((unsigned long int) 0xf << (HASHWORDBITS - 4));
-		if (g != 0) {
-			hval ^= g >> (HASHWORDBITS - 8);
-			hval ^= g;
-		}
-	}
-	return hval;
-}
-
-int table_add (w_table_t *table, unsigned int tlen, unsigned int mask, char *name, void *data)
-{
-	w_node_t *node;
-	w_table_t *tmp;
-	unsigned int o;
-	unsigned int l;
-	unsigned int mlen;
-	unsigned long int h;
-	for (l = 32; l > 0; l--) {
-		if (mask & (1 << (l - 1))) {
-			break;
-		}
-	}
-	mlen = l;
-	h = hash_string(name);
-	tmp = table;
-	for (l = 0; l < tlen; l++) {
-		o = h & mask;
-		if (tmp[o].table == NULL) {
-			tmp[o].table = (w_table_t *) s_malloc(sizeof(w_table_t) * (mask + 1));
-			memset(tmp[o].table, 0, sizeof(w_table_t) * (mask + 1));
-		}
-		tmp = tmp[o].table;
-		h >>= mlen;
-		if (h == 0) {
-			break;
-		}
-	}
-	node = (w_node_t *) s_malloc(sizeof(w_node_t));
-	memset(node, 0, sizeof(w_node_t));
-	node->key = h;
-	h &= mask;
-	node->name = strdup(name);
-	node->data = data;
-	node->table = &tmp[h];
-	node->next = tmp[h].node;
-	if (node->next) {
-		node->next->prev = node;
-	}
-	tmp[h].node = node;
-	return 0;
-}
-
-w_node_t * table_get_node (w_table_t *table, unsigned int tlen, unsigned int mask, char *name)
-{
-	w_node_t *node;
-	w_table_t *tmp;
-	unsigned int o;
-	unsigned int l;
-	unsigned int mlen;
-	unsigned long int h;
-	for (l = 32; l > 0; l--) {
-		if (mask & (1 << (l - 1))) {
-			break;
-		}
-	}
-	mlen = l;
-	h = hash_string(name);
-	tmp = table;
-	for (l = 0; l < tlen; l++) {
-		o = h & mask;
-		if (tmp[o].table == NULL) {
-			return NULL;
-		}
-		tmp = tmp[o].table;
-		h >>= mlen;
-		if (h == 0) {
-			break;
-		}
-	}
-	node = tmp[h & mask].node;
-	while (node) {
-		if (node->key == h &&
-		    cmp_string(node->name, name) == 0) {
-			return node;
-		}
-		node = node->next;
-	}
-	return NULL;
-}
-
-void * table_get_data (w_table_t *table, unsigned int tlen, unsigned int mask, char *name)
-{
-	w_node_t *node;
-	node = table_get_node(table, tlen, mask, name);
-	if (node) {
-		return node->data;
-	} else {
-		return NULL;
-	} 
-}
-
-int table_del (w_table_t *table, unsigned int tlen, unsigned int mask, char *name)
-{
-	w_node_t *node;
-	node = table_get_node(table, tlen, mask, name);
-	if (node == NULL) {
-		return -1;
-	}
-	if (node->prev) {
-		node->prev->next = node->next;
-	} else {
-		node->table->node = node->next;
-	}
-	s_free(node->name);
-	s_free(node);
-	return 0;
-}
-
-int table_print (w_table_t *table, unsigned int len)
-{
-	unsigned int i;
-	w_node_t *node;
-	if (table == NULL) {
-		return -1;
-	}
-	for (i = 0; i < len; i++) {
-		table_print(table[i].table, len);
-		node = table[i].node;
-		while (node) {
-			printf("%s\n", node->name);
-			node = node->next;
-		}
-	}
-	return 0;
-}
-
-int table_init (w_table_t **table, unsigned int size)
-{
-	w_table_t *tbl;
-	tbl = (w_table_t *) s_malloc(sizeof(w_table_t) * size);
-	memset(tbl, 0, sizeof(w_table_t) * size);
-	*table = tbl;
-	return 0;
-}
-
-int table_uninit (w_table_t *table, unsigned int len)
-{
-	w_node_t *tmp;
-	w_node_t *node;
-	unsigned int i;
-	if (table == NULL) {
-		return -1;
-	}
-	for (i = 0; i < len; i++) {
-		table_uninit(table[i].table, len);
-		node = table[i].node;
-		while (node) {
-			tmp = node;
-			node = node->next;
-			s_free(tmp->name);
-			s_free(tmp);
-		}
-	}
-	s_free(table);
-	return 0;
-}
-
 int main (int argc, char *argv[])
 {
 	unsigned int i;
@@ -260,15 +57,15 @@ int main (int argc, char *argv[])
 		char *hex;
 		unsigned int option_index = 0;
 		struct option long_options[] = {
-			{"tables", 1, 0, 0},
+			{"depth", 1, 0, 0},
 			{"mask", 1, 0, 0},
 			{"help", 0, 0, 0},
 			{0, 0, 0, 0},
 		};
-		while ((c = getopt_long(argc, argv, "t:m:h", long_options, &option_index)) != -1) {
+		while ((c = getopt_long(argc, argv, "d:m:h", long_options, &option_index)) != -1) {
 			switch (c) {
 				case 0:
-					if (strcmp("tables", long_options[option_index].name) == 0) {
+					if (strcmp("depth", long_options[option_index].name) == 0) {
 						goto option_tables;
 					} else if (strcmp("mask", long_options[option_index].name) == 0) {
 						goto option_mask;
@@ -284,7 +81,7 @@ option_mask:
 						widgetr->mask = atoi(optarg);
 					}
 					break;
-				case 't':
+				case 'd':
 option_tables:
 					widgetr->depth = atoi(optarg);
 					break;
