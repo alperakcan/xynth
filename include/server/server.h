46a4e5d66939cc093775c298fb0ea124fcd6c2e5
Alper Akcan <alper.akcan@gmail.com>
2006-12-26 09:20:19 +0000

    ...

--- include/server/server.h
+++ include/server/server.h
@@ -40,57 +40,7 @@ typedef struct s_server_s s_server_t;
 #include "server/window_handler.h"
 #include "server/window_move_resize.h"
 #include "server/theme.h"
-
-typedef enum {
-	VIDEO_INPUT_NONE,
-	VIDEO_INPUT_MOUSE,
-	VIDEO_INPUT_KEYBD,
-	VIDEO_INPUT_IRR,
-} VIDEO_INPUT;
-
-struct s_video_input_data_mouse_s {
-	int x;
-	int y;
-	int buttons;
-};
-
-struct s_video_input_data_keybd_s {
-	int state;
-	int button;
-	int keycode;
-	int scancode;
-	int ascii;
-};
-
-union s_video_input_data_u {
-	s_video_input_data_mouse_t mouse;
-	s_video_input_data_keybd_t keybd;
-	s_video_input_data_keybd_t irr;
-};
-
-struct s_video_input_s {
-	VIDEO_INPUT type;
-	int (*init) (s_server_conf_t *cfg);
-	int (*update) (s_video_input_data_t *idata);
-	void (*uninit) (void);
-};
-
-struct s_video_driver_s {
-	char *driver;
-	char *device;
-
-	s_video_input_t **input;
-
-	int (*server_init) (s_server_conf_t *cfg);
-	void (*server_uninit) (void);
-	void (*server_goto_back) (void);
-	void (*server_comefrom_back) (void);
-	void (*server_restore) (void);
-	void (*server_surface_update) (s_rect_t *coor);
-	void (*server_fullscreen) (void);
-
-	void *driver_data;
-};
+#include "server/video.h"
 
 struct s_server_conf_s {
 	struct {
