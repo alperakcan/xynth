f19179a971011285b948bb27a11b8f2064220849
Alper Akcan <alper.akcan@gmail.com>
2007-02-07 11:11:59 +0000

    ...

--- src/widget/frame.c
+++ src/widget/frame.c
@@ -14,6 +14,7 @@
  ***************************************************************************/
 
 #include <stdarg.h>
+#include <unistd.h>
 
 #include "../lib/xynth_.h"
 #include "widget.h"
@@ -62,6 +63,11 @@ int w_frame_set_image (w_object_t *object, unsigned int style, unsigned int rota
         w_frame_t *frame;
 	w_frame_image_t *fimg;
         frame = (w_frame_t *) object->data[OBJECT_FRAME];
+	for (pos = 0; pos < nimgs; pos++) {
+		if (access(imgs[pos], R_OK)) {
+			return -1;
+		}
+	}
 	for (pos = 0; !s_list_eol(frame->images, pos); pos++) {
 		fimg = (w_frame_image_t *) s_list_get(frame->images, pos);
 		if (fimg->style == style) {
