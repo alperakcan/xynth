c92defbd58d5a9f473e0823b47adca963e2476be
anhanguera <anhanguera@777c6629-d510-0410-a57e-d487f2a3d22b>
2007-03-19 09:00:38 +0000

    code cleanup

--- trunk/src/widget/widgetc/widgetc.c
+++ trunk/src/widget/widgetc/widgetc.c
@@ -22,9 +22,8 @@
 
 #include "list.h"
 #include "node.h"
-#include "lang.h"
-#include "gen_code.h"
-#include "gen_header.h"
+#include "code.h"
+#include "header.h"
 #include "parse.h"
 
 static FILE *g_source = NULL;
@@ -34,21 +33,7 @@ static char *g_source_name = NULL;
 static char *g_header_name = NULL;
 
 static void node_generate_element (node_t *node, node_t *s_node);
-static void node_generate_localization (node_t *node);
 static void node_generate_sources (node_t *node);
-
-static void node_generate_localization (node_t *node)
-{
-	node_t *lang;
-	node_t *local;
-	while ((local = node_get_node(node, "localization")) != NULL) {
-		while ((lang = node_get_node(local, "language")) != NULL) {
-			lang_generate(lang, g_name);
-			lang->dontparse = 1;
-		}
-		local->dontparse = 1;
-	}
-}
 
 static void node_generate_sources (node_t *node)
 {
@@ -60,9 +45,9 @@ static void node_generate_sources (node_t *node)
 	        "#include <xynth.h>\n"
 	        "#include <widget.h>\n"
 	        "\n");
-	node_generate_header(node, g_header);
+	header_generate(node, g_header);
 	fprintf(g_header, "\n");
-	node_generate_function(node, g_header);
+	header_generate_function(node, g_header);
 	fprintf(g_source,
 	        "\n"
 	        "#include \"%s\"\n"
@@ -71,7 +56,7 @@ static void node_generate_sources (node_t *node)
 	        "{\n"
 	        "srand(time(NULL));\n",
 	        g_header_name);
-	node_generate_code(node, g_source, g_name);
+	code_generate(node, g_source, g_name);
 	fprintf(g_source,
 	        "return 0;\n"
 	        "}\n");
@@ -121,10 +106,8 @@ int main (int argc, char **argv)
 	char *varf = NULL;
 	char *vars = NULL;
 	xml_data_t *xdata;
-	int localization = 0;
-	int sources      = 0;
 	
-	while ((c = getopt(argc, argv, "s:f:o:clh")) != -1) {
+	while ((c = getopt(argc, argv, "s:f:o:h")) != -1) {
 		switch (c) {
 			case 'f':
 				varf = optarg;
@@ -135,25 +118,18 @@ int main (int argc, char **argv)
 			case 's':
 				vars = optarg;
 				break;
-			case 'l':
-				localization = 1;
-				break;
-			case 'c':
-				sources = 1;
-				break;
-usage:			case 'h':
+			case 'h':
 			default:
-				printf("%s -f input_file -o output_name -c -l\n"
+usage:				printf("%s -f input_file -o output_name [-s style]\n"
 				       "\t-f : input file"
 				       "\t-o : output name"
-				       "\t-c : output source file"
-				       "\t-l : output localization file", argv[0]);
+				       "\t-s : style sheet\n", argv[0]);
 				exit(1);
 				break;
 		}
 	}
 	
-	if (sources == 0 && localization == 0) {
+	if (varf == NULL || varo == NULL) {
 		goto usage;
 	}
 	
@@ -178,32 +154,25 @@ usage:			case 'h':
 		g_source_name = (char *) malloc(strlen(varo) + 20);
 		g_header_name = (char *) malloc(strlen(varo) + 20);
 		g_name = strdup(varo);
-		if (sources == 1) {
-			sprintf(g_source_name, "%s_xml.c", varo);
-			sprintf(g_header_name, "%s_xml.h", varo);
-			g_source = fopen(g_source_name, "w+");
-			g_header = fopen(g_header_name, "w+");
-		}
+		sprintf(g_source_name, "%s_xml.c", varo);
+		sprintf(g_header_name, "%s_xml.h", varo);
+		g_source = fopen(g_source_name, "w+");
+		g_header = fopen(g_header_name, "w+");
 	} else {
 		goto usage;
 	}
 	
 	node_generate_element(xdata->root, xdata->elem);
 
-	if (localization)
-		node_generate_localization(xdata->root);
-	if (sources)
-		node_generate_sources(xdata->root);
+	node_generate_sources(xdata->root);
 
 	for (root = xdata->root; root && root->parent; root = root->parent);
 	node_uninit(root);
 	for (root = xdata->elem; root && root->parent; root = root->parent);
 	node_uninit(root);
 
-	if (sources) {
-		fclose(g_source);
-		fclose(g_header);
-	}
+	fclose(g_source);
+	fclose(g_header);
 	
 	free(xdata);
 	free(g_name);
