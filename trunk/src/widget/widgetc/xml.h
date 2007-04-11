6925e0213dd2e08aa6d8d04194d3ec15d4d9d597
anhanguera <anhanguera@777c6629-d510-0410-a57e-d487f2a3d22b>
2007-04-11 12:25:57 +0000

    mode doxy doc

--- trunk/src/widget/widgetc/xml.h
+++ trunk/src/widget/widgetc/xml.h
@@ -15,7 +15,6 @@
 
 typedef struct s_xml_node_attr_s s_xml_node_attr_t;
 typedef struct s_xml_node_s s_xml_node_t;
-typedef struct s_xml_data_s s_xml_data_t;
 
 struct s_xml_node_attr_s {
 	char *name;
@@ -32,15 +31,6 @@ struct s_xml_node_s {
 	int dontparse;
 };
 
-struct s_xml_data_s {
-	char *path;
-	s_xml_node_t *active;
-	s_xml_node_t *root;
-	s_xml_node_t *elem;
-};
-
-int s_xml_node_path_normalize (char *out, int len);
-s_xml_node_t * s_xml_node_get_path_ (s_xml_node_t *node, char *path);
 s_xml_node_t * s_xml_node_get_path (s_xml_node_t *node, char *path);
 char * s_xml_node_get_value (s_xml_node_t *node);
 char * s_xml_node_get_path_value (s_xml_node_t *node, char *path);
@@ -53,9 +43,5 @@ int s_xml_node_attr_init (s_xml_node_attr_t **attr);
 int s_xml_node_attr_uninit (s_xml_node_attr_t *attr);
 int s_xml_node_init (s_xml_node_t **node);
 int s_xml_node_uninit (s_xml_node_t *node);
-void s_xml_parse_start (void *xdata, const char *el, const char **xattr);
-void s_xml_parse_end (void *xdata, const char *el);
-void s_xml_parse_character_fixup (char *out);
-void s_xml_parse_character (void *xdata, const char *txt, int txtlen);
 int s_xml_parse_buffer (s_xml_node_t **node, char *buffer, unsigned int len);
 int s_xml_parse_file (s_xml_node_t **node, char *file);
