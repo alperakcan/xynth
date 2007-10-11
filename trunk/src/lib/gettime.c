20de55e2742aa4767c45eef50f7b444e4a854bb4
anhanguera <anhanguera@777c6629-d510-0410-a57e-d487f2a3d22b>
2007-10-11 12:03:51 +0000

    mingw version update

--- trunk/src/lib/gettime.c
+++ trunk/src/lib/gettime.c
@@ -47,11 +47,6 @@ unsigned int sleep (unsigned int sec)
 #define EPOCHFILETIME (116444736000000000LL)
 #endif
 
-struct timezone {
-	int tz_minuteswest;
-	int tz_dsttime;
-};
-
 void usleep (unsigned long msec)
 {
 	Sleep(msec);
@@ -60,34 +55,6 @@ void usleep (unsigned long msec)
 unsigned int sleep (unsigned int sec)
 {
 	usleep(sec * 1000 * 1000);
-	return 0;
-}
-
-int gettimeofday (struct timeval *tv, struct timezone *tz)
-{
-	FILETIME ft;
-	LARGE_INTEGER li;
-	__int64 t;
-	static int tzflag;
-	
-	if (tv) {
-		GetSystemTimeAsFileTime(&ft);
-		li.LowPart = ft.dwLowDateTime;
-		li.HighPart = ft.dwHighDateTime;
-		t = li.QuadPart;
-		t -= EPOCHFILETIME;
-		t /= 10;
-		tv->tv_sec = (long) (t / 1000000);
-		tv->tv_usec = (long) (t % 1000000);
-	}
-	if (tz) {
-		if (!tzflag) {
-			_tzset();
-			tzflag++;
-		}
-		tz->tz_minuteswest = _timezone / 60;
-		tz->tz_dsttime = _daylight;
-	}
 	return 0;
 }
 
