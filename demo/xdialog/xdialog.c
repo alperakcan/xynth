29daccc22a841a5e1170c54d2478d4cc7cc982e7
Alper Akcan <alper.akcan@gmail.com>
2007-10-23 11:39:47 +0000

    ...

--- demo/xdialog/xdialog.c
+++ demo/xdialog/xdialog.c
@@ -1,3 +1,18 @@
+/***************************************************************************
+    begin                : Tue Sep 22 2007
+    copyright            : (C) 2007 by Alper Akcan
+    email                : distchx@yahoo.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,9 +22,11 @@
 #include "xdialog.h"
 
 extern xd_widget_t xd_msgbox;
+extern xd_widget_t xd_yesno;
 
 xd_widget_t *xd_widgets[] = {
 	&xd_msgbox,
+	&xd_yesno,
 };
 
 struct option xd_options[] = {
--- demo/xdialog/yesno.c
+++ demo/xdialog/yesno.c
@@ -24,13 +24,21 @@
 static const int pbutton_width = 50;
 static const int pbutton_height = 25;
 static const int object_space = 5;
+static int xd_yesno_return = 0;
 
-void xd_msgbox_pbutton_clicked (w_pushbutton_t *pbutton)
+void xd_yesno_pbutton_yes_clicked (w_pushbutton_t *pbutton)
 {
+	xd_yesno_return = 0;
 	w_window_quit(pbutton->object->window);
 }
 
-int xd_msgbox_callback (int argc, char *argv[])
+void xd_yesno_pbutton_no_clicked (w_pushbutton_t *pbutton)
+{
+	xd_yesno_return = 1;
+	w_window_quit(pbutton->object->window);
+}
+
+int xd_yesno_callback (int argc, char *argv[])
 {
 	int x;
 	int y;
@@ -65,20 +73,26 @@ int xd_msgbox_callback (int argc, char *argv[])
 	w_object_show(textbox->object);
 	
 	w_pushbutton_init(window, &pbutton, frame->object);
-	w_pushbutton_set_text(pbutton->object, "OK");
-	w_pushbutton_set_clicked(pbutton->object, xd_msgbox_pbutton_clicked);
-	w_object_move(pbutton->object, (width - pbutton_width) / 2, height - pbutton_height - object_space, pbutton_width, pbutton_height);
+	w_pushbutton_set_text(pbutton->object, "Yes");
+	w_pushbutton_set_clicked(pbutton->object, xd_yesno_pbutton_yes_clicked);
+	w_object_move(pbutton->object, (width - ((pbutton_width * 2) + object_space)) / 2, height - pbutton_height - object_space, pbutton_width, pbutton_height);
+	w_object_show(pbutton->object);
+	
+	w_pushbutton_init(window, &pbutton, frame->object);
+	w_pushbutton_set_text(pbutton->object, "No");
+	w_pushbutton_set_clicked(pbutton->object, xd_yesno_pbutton_no_clicked);
+	w_object_move(pbutton->object, ((width - ((pbutton_width * 2) + object_space)) / 2) + pbutton_width + object_space, height - pbutton_height - object_space, pbutton_width, pbutton_height);
 	w_object_show(pbutton->object);
 	
 	w_object_show(window->object);
 	w_window_main(window);
 	
-	return 0;
+	return xd_yesno_return;
 }
 
-xd_widget_t xd_msgbox = {
-	"msgbox",
+xd_widget_t xd_yesno = {
+	"yesno",
 	3,
-	xd_msgbox_callback,
+	xd_yesno_callback,
 	"<text> <height> <width>",
 };
