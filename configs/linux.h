218fd3550985d44646fc6b29848f462c46d7431c
Alper Akcan <alper.akcan@gmail.com>
2007-12-06 15:57:34 +0000

    ...

--- configs/linux.h
+++ configs/linux.h
@@ -1,112 +1,62 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
 /*
- * Automatically generated header file: don't edit
+ * Automatically generated C config: don't edit
+ * Xynth version: 
+ * Thu Dec  6 15:49:41 2007
  */
-
-/* Version Number */
-#define XYNTH_VERSION "(null)"
-#define XYNTH_BUILDTIME "(null)"
-
-#define CONFIG_DEBUG 1
-#undef CONFIG_SINGLE_APPLICATION
-#define CONFIG_PLATFORM_LINUX 1
-#undef CONFIG_PLATFORM_PSPDEV
-#undef CONFIG_PLATFORM_MINGW
-#undef CONFIG_PLATFORM_GP2X
-
-/*
- * Video Drivers
- */
+#define AUTOCONF_INCLUDED
+#define CONFIG_WIDGET_STATIC 1
+#define CONFIG_PATH_SHARE "share/xynth"
+#define CONFIG_DEMO_TIMER 1
 #define CONFIG_VIDEO_VESA 1
-#define CONFIG_VIDEO_SVGALIB 1
-#define CONFIG_VIDEO_FBDEV 1
-#define CONFIG_VIDEO_SDL 1
-#undef CONFIG_VIDEO_CACA
-#undef CONFIG_VIDEO_PSPDEV
-#undef CONFIG_VIDEO_GDI
-#define CONFIG_VIDEO_NULL 1
-
-/*
- * Video Driver Helper
- */
-#define CONFIG_VIDEO_HELPER 1
-#define CONFIG_VIDEO_HELPER_MODES 1
 #define CONFIG_VIDEO_HELPER_CONSOLE 1
-#define CONFIG_VIDEO_HELPER_KEYBOARD 1
-#define CONFIG_VIDEO_HELPER_MOUSE 1
-#define CONFIG_VIDEO_HELPER_MTRR 1
-#define CONFIG_THREAD_PTHREADS 1
-#undef CONFIG_THREAD_WINDOWS
-#undef CONFIG_THREAD_PSPDEV
-
-/*
- * Communication Method
- */
+#define CONFIG_DEMO_APPLICATIONS 1
+#define CONFIG_VIDEO_NULL 1
+#define CONFIG_PATH_FONTS "share/xynth/fonts"
 #define CONFIG_PIPE_POSIX 1
-#undef CONFIG_PIPE_MEMORY
-#define CONFIG_SOCKET_POSIX 1
-#undef CONFIG_SOCKET_PIPE
-#define CONFIG_POLL_POLL 1
-#undef CONFIG_POLL_SELECT
 #define CONFIG_IPC_TCP 1
 #define CONFIG_IPC_UDS 1
-#undef CONFIG_IPC_PIPE
-
-/*
- * Image Support
- */
-#define CONFIG_IMAGE_PNG 1
-#define CONFIG_IMAGE_GIF 1
-#define CONFIG_IMAGE_XPM 1
-
-/*
- * Theme support
- */
-#define CONFIG_THEME_PLUGIN 1
-#define CONFIG_THEME_KERAMIK 1
-#define CONFIG_THEME_MICROGUI 1
-#define CONFIG_THEME_SILVERADO 1
-
-/*
- * Widget Library
- */
-#define CONFIG_WIDGET_LIBRARY 1
-#define CONFIG_WIDGET_STATIC 1
-#define CONFIG_WIDGET_SHARED 1
 #define CONFIG_WIDGET_WIDGETC 1
-#define CONFIG_WIDGET_WIDGETR 1
-
-/*
- * Demo Applications
- */
-#define CONFIG_DEMO_APPLICATIONS 1
-#define CONFIG_DEMO_CHILD 1
-#define CONFIG_DEMO_DESKTOP 1
-#define CONFIG_DEMO_HIDE 1
-#define CONFIG_DEMO_LOAD 1
-#define CONFIG_DEMO_SIMPLE 1
-#define CONFIG_DEMO_OBJECT 1
+#define CONFIG_THEME_MICROGUI 1
 #define CONFIG_DEMO_ONSCREENKEYBOARD 1
-#define CONFIG_DEMO_TEMP 1
-#define CONFIG_DEMO_TERM 1
-#define CONFIG_DEMO_TIMER 1
-#define CONFIG_DEMO_LOGOUT 1
-#define CONFIG_DEMO_IMAGE 1
-#define CONFIG_DEMO_WIDGETX 1
-#define CONFIG_DEMO_XDIALOG 1
-
-/*
- * Paths
- */
-#define CONFIG_PATH_INSTALL "/usr/local/"
-#define CONFIG_PATH_BIN "bin"
-#define CONFIG_PATH_LIB "lib"
-#define CONFIG_PATH_INCLUDE "include"
-#define CONFIG_PATH_SHARE "share/xynth"
-#define CONFIG_PATH_FONTS "share/xynth/fonts"
-#define CONFIG_PATH_CONFIGS "share/xynth/configs"
+#define CONFIG_WIDGET_LIBRARY 1
+#define CONFIG_WIDGET_WIDGETR 1
+#define CONFIG_VIDEO_HELPER_MTRR 1
+#define CONFIG_DEMO_OBJECT 1
+#define CONFIG_VIDEO_FBDEV 1
+#define CONFIG_VIDEO_HELPER_KEYBOARD 1
+#define CONFIG_DEMO_SIMPLE 1
 #define CONFIG_PATH_THEMES "share/xynth/themes"
-
-#endif /* CONFIG_H */
+#define CONFIG_PLATFORM_LINUX 1
+#define CONFIG_DEMO_LOGOUT 1
+#define CONFIG_PATH_LIB "lib"
+#define CONFIG_PATH_BIN "bin"
+#define CONFIG_DEBUG 1
+#define CONFIG_VIDEO_HELPER_MODES 1
+#define CONFIG_THREAD_PTHREADS 1
+#define CONFIG_THEME_SILVERADO 1
+#define CONFIG_VIDEO_HELPER_MOUSE 1
+#define CONFIG_DEMO_XDIALOG 1
+#define CONFIG_PATH_INCLUDE "include"
+#define CONFIG_PATH_CONFIGS "share/xynth/configs"
+#define CONFIG_VIDEO_HELPER 1
+#define CONFIG_IMAGE_GIF 1
+#define CONFIG_DEMO_HIDE 1
+#define CONFIG_DEMO_DESKTOP 1
+#define CONFIG_DEMO_WIDGETX 1
+#define CONFIG_THEME_PLUGIN 1
+#define CONFIG_PATH_INSTALL "/usr/local/"
+#define CONFIG_DEMO_LOAD 1
+#define CONFIG_IMAGE_PNG 1
+#define CONFIG_IMAGE_XPM 1
+#define CONFIG_VIDEO_SDL 1
+#define CONFIG_DEMO_TEMP 1
+#define CONFIG_SOCKET_POSIX 1
+#define CONFIG_DEMO_TERM 1
+#define UNAME_RELEASE "2.6.22.8-distch"
+#define CONFIG_POLL_POLL 1
+#define CONFIG_THEME_KERAMIK 1
+#define CONFIG_WIDGET_SHARED 1
+#define CONFIG_VIDEO_SVGALIB 1
+#define CONFIG_CONFIGURED 1
+#define CONFIG_DEMO_IMAGE 1
+#define CONFIG_DEMO_CHILD 1
