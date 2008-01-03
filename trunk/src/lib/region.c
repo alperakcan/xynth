f5009ff3311aa5232e0d406ce0c6e0b1be9c6e64
anhanguera <anhanguera@777c6629-d510-0410-a57e-d487f2a3d22b>
2008-01-03 12:12:16 +0000

    region

--- trunk/src/lib/region.c
+++ trunk/src/lib/region.c
@@ -40,28 +40,25 @@ int s_region_destroy (s_region_t *region)
 	return 0;
 }
 
-int s_region_addrect (s_region_t *region, s_rect_t *rect)
+int s_region_extents_calculate (s_region_t *region)
 {
 	int n;
 	s_rect_t *e;
 	s_rect_t *r;
 	
-	if (region == NULL) {
-		goto err0;
-	}
-	region->rects = (s_rect_t *) realloc(region->rects, sizeof(s_rect_t) * (region->nrects + 1));
-	if (region->rects != NULL) {
-		region->rects[region->nrects] = *rect;
-		region->nrects += 1;
-	} else {
-		goto err0;
-	}
-	
 	r = region->rects;
 	n = region->nrects;
 	e = &region->extents;
+	if (n == 0) {
+		e->x = 0;
+		e->y = 0;
+		e->w = 0;
+		e->h = 0;
+		return 0;
+	}
 	*e = *r;
-	while (n) {
+
+	while (n--) {
 		int ex2 = e->x + e->w;
 		int ey2 = e->y + e->h;
 		int rx2 = r->x + r->w;
@@ -72,10 +69,65 @@ int s_region_addrect (s_region_t *region, s_rect_t *rect)
 		e->w = MAX(ex2, rx2) - e->x;
 		e->h = MAX(ey2, ry2) - e->y;
 
-		n--;
 		r++;
 	}
 	
+	return 0;
+}
+
+int s_region_delrect (s_region_t *region, s_rect_t *rect)
+{
+	int n;
+	s_rect_t *r;
+	s_rect_t *rs;
+	if (region == NULL) {
+		goto err0;
+	}
+	for (n = 0; n < region->nrects; n++) {
+		r = &region->rects[n];
+		if (r->x == rect->x &&
+		    r->y == rect->y &&
+		    r->w == rect->w &&
+		    r->h == rect->h) {
+			if (region->nrects == 1) {
+				region->nrects = 0;
+				free(region->rects);
+				region->rects = NULL;
+				break;
+			}
+			rs = (s_rect_t *) malloc(sizeof(s_rect_t) * (region->nrects - 1));
+			if (rs == NULL) {
+				goto err0;
+			}
+			region->nrects -= 1;
+			if (n > 0) {
+				memcpy(rs, region->rects, n * sizeof(s_rect_t));
+			}
+			if (n < region->nrects) {
+				memcpy(rs + n, region->rects + n + 1, (region->nrects - n) * sizeof(s_rect_t));
+			}
+			free(region->rects);
+			region->rects = rs;
+		}
+	}
+	s_region_extents_calculate(region);
+	return 0;
+err0:	return -1;
+}
+
+int s_region_addrect (s_region_t *region, s_rect_t *rect)
+{
+	if (region == NULL) {
+		goto err0;
+	}
+	region->rects = (s_rect_t *) realloc(region->rects, sizeof(s_rect_t) * (region->nrects + 1));
+	if (region->rects != NULL) {
+		region->rects[region->nrects] = *rect;
+		region->nrects += 1;
+	} else {
+		goto err0;
+	}
+	s_region_extents_calculate(region);
 	return 0;
 err0:	return -1;
 }
@@ -103,4 +155,36 @@ int s_region_extents (s_region_t *region, s_rect_t *extents)
 		return 0;
 	}
 	return -1;
+}
+
+int s_region_copy (s_region_t **region, s_region_t *src)
+{
+	s_region_t *r;
+	r = (s_region_t *) malloc(sizeof(s_region_t));
+	if (r == NULL) {
+		goto err0;
+	}
+	memcpy(r, src, sizeof(s_region_t));
+	r->rects = (s_rect_t *) malloc(sizeof(s_rect_t) * r->nrects);
+	if (r->rects == NULL) {
+		goto err1;
+	}
+	memcpy(r->rects, src->rects, sizeof(s_rect_t) * r->nrects);
+	*region = r;
+	return 0;
+err1:	free(r);
+err0:	*region = NULL;
+	return -1;
+}
+
+int s_region_unify (s_region_t *region)
+{
+	s_region_extents_calculate(region);
+	return 0;
+}
+
+int s_region_combine (s_region_t *region)
+{
+	s_region_extents_calculate(region);
+	return 0;
 }
