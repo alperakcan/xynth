174a236d36435af3b66e045ea206fc5e61eca786
anhanguera <anhanguera@777c6629-d510-0410-a57e-d487f2a3d22b>
2008-01-07 13:01:23 +0000

    region wip

--- trunk/src/lib/region.c
+++ trunk/src/lib/region.c
@@ -31,12 +31,10 @@ err0:	*region = NULL;
 
 int s_region_destroy (s_region_t *region)
 {
-	if (region) {
-		if (region->rects) {
-			free(region->rects);
-		}
-		free(region);
+	if (region->rects) {
+		free(region->rects);
 	}
+	free(region);
 	return 0;
 }
 
@@ -102,9 +100,6 @@ int s_region_delrect (s_region_t *region, s_rect_t *rect)
 	int n;
 	s_rect_t *r;
 	s_rect_t *rs;
-	if (region == NULL) {
-		goto err0;
-	}
 	for (n = 0; n < region->nrects;) {
 		r = &region->rects[n];
 		if (!(r->x - rect->x || r->y - rect->y ||
@@ -117,7 +112,7 @@ int s_region_delrect (s_region_t *region, s_rect_t *rect)
 			}
 			rs = (s_rect_t *) malloc(sizeof(s_rect_t) * (region->nrects - 1));
 			if (rs == NULL) {
-				goto err0;
+				return -1;
 			}
 			region->nrects -= 1;
 			if (n > 0) {
@@ -135,16 +130,12 @@ int s_region_delrect (s_region_t *region, s_rect_t *rect)
 		}
 	}
 	return 0;
-err0:	return -1;
 }
 
 int s_region_addrect (s_region_t *region, s_rect_t *rect)
 {
 	int n;
 	s_rect_t *r;
-	if (region == NULL) {
-		goto err0;
-	}
 	if (rect->w <= 0 || rect->h <= 0) {
 		return 0;
 	}
@@ -162,7 +153,7 @@ int s_region_addrect (s_region_t *region, s_rect_t *rect)
 		region->rects[region->nrects] = *rect;
 		region->nrects += 1;
 	} else {
-		goto err0;
+		return -1;
 	}
 	if (region->nrects == 1) {
 		region->extents = *rect;
@@ -170,32 +161,22 @@ int s_region_addrect (s_region_t *region, s_rect_t *rect)
 		s_region_rect_union(&region->extents, rect, &region->extents);
 	}
 	return 0;
-err0:	return -1;
 }
 
 int s_region_num_rectangles (s_region_t *region)
 {
-	if (region) {
-		return region->nrects;
-	}
-	return 0;
+	return region->nrects;
 }
 
 s_rect_t * s_region_rectangles (s_region_t *region)
 {
-	if (region) {
-		return region->rects;
-	}
-	return NULL;
+	return region->rects;
 }
 
 int s_region_extents (s_region_t *region, s_rect_t *extents)
 {
-	if (region) {
-		*extents = region->extents;
-		return 0;
-	}
-	return -1;
+	*extents = region->extents;
+	return 0;
 }
 
 int s_region_copy (s_region_t **region, s_region_t *src)
