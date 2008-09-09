b4287905d064b2f00369b24e3ad4b5201bfe814e
Alper Akcan <alper.akcan@gmail.com>
2008-09-09 16:38:56 +0000

    ...

---
+++ src/widget/toolbar.c
@@ -0,0 +1,15 @@
+/***************************************************************************
+    begin                : Tue Sep 09 2008
+    copyright            : (C) 2008 by Alper Akcan
+    email                : alper.akcan@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2.1 of the  *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
